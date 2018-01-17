a5ec7213649d641a34749f22024c031acac21c09
luis <luis.barcellos@hotmail.com>
2018-01-17 14:24:10 -0200

    Adicionando testes de integração post.

--- mongodb-service-app/src/test/java/br/com/mongodb/contract/v1/get/ProdutoControllerTest.java
+++ mongodb-service-app/src/test/java/br/com/mongodb/contract/v1/get/ProdutoControllerTest.java
@@ -2,6 +2,8 @@ package br.com.mongodb.contract.v1.get;
 
 import br.com.mongodb.contract.v1.ProdutoController;
 import br.com.mongodb.contract.v1.stub.ProdutoStub;
+import br.com.mongodb.database.ProdutoRepository;
+import br.com.mongodb.exception.ProdutoNotFoundException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,5 +35,15 @@ public class ProdutoControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(status().is(200))
                 .andReturn().getRequest().equals(ProdutoStub.getProduto());
+    }
+
+    @Test
+    public void buscarProdutoNotFound() throws Exception{
+        when(produtoController.buscarProduto(21332L)).thenThrow(ProdutoNotFoundException.class);
+
+        this.mockMvc.perform(get("/produto/21332"))
+                .andDo(print())
+                .andExpect(status().isNotFound())
+                .andExpect(status().is(404));
     }
 }
--- mongodb-service-app/src/test/java/br/com/mongodb/contract/v1/post/ProdutoControllerTest.java
+++ mongodb-service-app/src/test/java/br/com/mongodb/contract/v1/post/ProdutoControllerTest.java
@@ -1,17 +1,28 @@
 package br.com.mongodb.contract.v1.post;
 
 import br.com.mongodb.contract.v1.ProdutoController;
+import br.com.mongodb.contract.v1.model.Produto;
 import br.com.mongodb.contract.v1.stub.ProdutoStub;
+import br.com.mongodb.exception.ProdutoNotFoundException;
+import br.com.mongodb.exception.ProdutoRepetidoException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -21,17 +32,33 @@ public class ProdutoControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @MockBean
     private ProdutoController produtoController;
 
     @Test
-    public void buscarProdutoSucesso() throws Exception{
-        when(produtoController.buscarProduto(21332L)).thenReturn(ProdutoStub.getProduto());
+    public void inserirProdutoSucesso() throws Exception{
+        String json  = objectMapper.writeValueAsString(ProdutoStub.getProduto());
 
-        this.mockMvc.perform(get("/produto/21332"))
+        this.mockMvc.perform(post("/produto")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(json))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(status().is(200))
-                .andReturn().getRequest().equals(ProdutoStub.getProduto());
+                .andExpect(status().is(200));
+    }
+
+    @Test
+    public void inserirProdutoSemCodigo() throws Exception{
+        String json  = objectMapper.writeValueAsString(ProdutoStub.getProdutoSemCodigo());
+
+        this.mockMvc.perform(post("/produto")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(json))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(status().is(400));
     }
 }
